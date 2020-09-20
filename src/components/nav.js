6f3f1b30c51c891737047b70a9429829a968e326
Godwill Barasa <iamgodwillb@gmail.com>
2020-09-20 23:25:41 +0300

    f-structure 23:23

---
+++ src/components/nav.js
@@ -0,0 +1,110 @@
+import React from 'react'
+
+function nav() {
+	return (
+		<div>
+			<div id="royal_preloader"></div>
+
+			<div class="top-border">
+				<div class="w-row">
+					<div class="w-col w-col-9 w-clearfix">
+						 <div data-ix="show-menu-type-3" class="menu-type-2">
+						 	<div class="w-embed">
+						 		<button class="c-hamburger c-hamburger--htx">
+					              <span>toggle menu</span>
+					            </button>
+					        </div>
+					     </div>
+					</div>  
+				</div>
+			</div>
+
+			<div class="left-border"></div>
+			<div class="right-border"></div>
+			<div class="bottom-border"></div>
+
+    <div class="w-container port-contatiner">
+    	<nav class="navigation-wrapper">
+	        <ul class="w-list-unstyled ul-nav">
+	          <li><a href="portfolio.html" class="w-inline-block nav-link animsition-link"><h1 class="margin-0">portfolio</h1></a>
+	          </li>
+	          <li><a href="biography.html" class="w-inline-block nav-link animsition-link"><h1 class="margin-0">biography</h1></a>
+	          </li>
+	          <li><a href="thougts.html" class="w-inline-block nav-link animsition-link"><h1 class="margin-0">Thougts</h1></a>
+	          </li>
+	          <li><a href="contact-3.html" class="w-inline-block nav-link animsition-link"><h1 class="margin-0">contact</h1></a>
+	          </li>
+	        </ul>
+        </nav>
+    </div>
+    <div class="video-overlay more-darker">
+      <div class="info-menu-type-3">
+        <div class="w-row">
+          <div class="w-col w-col-6">
+            <div class="align-left">
+              <a href="#" class="w-inline-block social-icon facebook social-white">
+                <i class="fa fa-facebook"></i>
+              </a>
+              <a href="#" class="w-inline-block social-icon twitter social-white">
+                <i class="fa fa-twitter"></i>
+              </a>
+              <a href="#" class="w-inline-block social-icon behance social-white">
+                <i class="fa fa-instagram"></i>
+              </a>
+              <a href="#" class="w-inline-block social-icon instagram social-white">
+                <div class="w-embed"><i class="fa fa-behance"></i>
+                </div>
+              </a>
+            </div>
+          </div>
+          <div class="w-col w-col-6">
+            <div class="w-hidden-medium w-hidden-small w-hidden-tiny title-top-border">
+              <div>GODWILL BARASA COPYRIGHT 2020.</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  
+  
+
+		  <div class="w-hidden-main">
+		    <div class="responsive-menu">
+		      <div class="w-container">
+		      	<div data-ix="open-responsive-menu" class="hamburger">
+		          <div class="w-embed">
+		            <button class="c-hamburger c-hamburger--htx">
+		              <span>toggle menu</span>
+		            </button>
+		          </div>
+		        </div>
+		      </div>
+		    </div>
+		    <div class="responsive-nav">
+		      <div class="w-container">
+		        <ul class="w-list-unstyled dd-big-res">
+		          <li>
+		            <div class="w-clearfix nav-res-link"><a href="portfolio.html" class="res-txt animsition-link">portfolio</a>
+		            </div>
+		          </li>
+		          <li>
+		            <div class="w-clearfix nav-res-link"><a href="biography.html" class="res-txt animsition-link">biography</a>
+		            </div>
+		          </li>
+		          <li>
+		            <div class="w-clearfix nav-res-link"><a href="thougts.html" class="res-txt animsition-link">thougts</a>
+		            </div>
+		          </li>
+		          <li>
+		            <div class="w-clearfix nav-res-link"><a href="contact-3.html" class="res-txt animsition-link">Contact</a>
+		            </div>
+		          </li>
+		        </ul>
+		      </div>
+		    </div>
+		  </div>
+	</div>
+	)
+}
+
+export default nav
